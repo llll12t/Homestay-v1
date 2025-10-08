c15908c5288572c041f8eb8f6c94d6786424301e
llll12t <147978645+llll12t@users.noreply.github.com>
2025-10-08 15:25:32 +0700

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,12 @@
+// config.js
+const CONFIG = {
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbz6M0KzJZBVV3r61igQTvucTn2JTKJK965xVjDdQ7fRXSh_MkG6SVzaSH3MFnyxuFC8/exec',
+  GAS_URL_PAYMENT: 'https://script.google.com/macros/s/AKfycbzNSxS8CsP2syyfXOM5odfj3BYt11qJHXMULIgWtcMDM6ABBmRWKEjhOPPM0cEtY4j-/exec' ,
+  GAS_URL_REVIEW: 'https://script.google.com/macros/s/AKfycbwvzvB7ZLt8b_UHImWdvTqGyE2fT4Yh4OKB4SFTRGxwjfYMT93-8QYgNYlwf3O9JAE/exec' ,
+  GAS_URL_ADMIN_HISTORY: 'https://script.google.com/macros/s/AKfycbyPljZ05WrGsoQK08sjgbt3MRD9TO1s12jVrTO5Py_3-WhyyHjIsdiOA0nDhdD34tCV/exec',
+  GAS_URL_ADMIN: 'https://script.google.com/macros/s/AKfycbzZ7QNIgh1I8mm0khQ4j-y2G0BGiUjHO3obmsnTokdSaencO3-BzPHUKIS8F6rP0Xf3OQ/exec' ,
+  
+  LIFF_ID: '2007106715-87nBmwNL',
+  LIFF_HISTORY:'2007106715-OYJEx946',
+  LIFF_REVIEW: '2007106715-AWxXWQ0P'
+};
